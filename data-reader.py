ce93bcc55ffef5b9da8bd1378c7b4782d9b02a0a
Phu Dang <dangnphu31@gmail.com>
2024-11-29 17:19:51 -0800

    update database

--- data-reader.py
+++ data-reader.py
@@ -23,18 +23,41 @@ async def websocket_to_postgresql():
             
             # filter for server room only
             if data.get('location') == 'Server Room':
-                store_to_database(data)
+                store_to_database(data, 'Server Room')
 
-def store_to_database(data):
+            # filter for san diego only
+            if data.get('location') == 'San Diego':
+                store_to_database(data, 'San Diego')
+
+            # filter for the computers
+            if data.get('system_id') != 'lab_lights':
+                store_to_database(data, 'pc')
+
+
+def store_to_database(data, tag):
     try:
         # Connect to Heroku PostgreSQL with Secure Sockets Layer enabled
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         cursor = conn.cursor()
 
         # Insert data to 'server_room_data' table
-        cursor.execute("""
-            INSERT INTO server_room_data (location, energy_consumption_kW, cpu_usage_percent, timestamp) VALUES (%s, %s, %s, to_timestamp(%s / 1000.0))
-        """, (data['location'], data['energy_consumption_kW'], data['cpu_usage_percent'], data['timestamp']))
+        if tag == 'Server Room':
+
+            cursor.execute("""
+                INSERT INTO server_room_data (location, energy_consumption_kW, cpu_usage_percent, timestamp) VALUES (%s, %s, %s, to_timestamp(%s / 1000.0))
+            """, (data['location'], data['energy_consumption_kW'], data['cpu_usage_percent'], data['timestamp']))
+            
+        elif tag == 'San Diego':
+            
+            cursor.execute("""
+                INSERT INTO sd_data (location, temperature, timestamp) VALUES (%s, %s, to_timestamp(%s / 1000.0))
+            """, (data['location'], data['temperature'], data['timestamp']))
+
+        elif tag == 'pc':
+
+            cursor.execute("""
+                INSERT INTO pc_data (id, cpuUsagePercent, memoryUsagePercent, gpuUsagePercent, timestamp) VALUES (%s, %s, %s, %s, to_timestamp(%s / 1000.0))
+            """, (data['system_id'], data['cpuUsagePercent'], data['memoryUsagePercent'], data['gpuUsagePercent'], data['time']))
 
         conn.commit()
         cursor.close()
